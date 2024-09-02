35311e2b83963ea5d4b829508858487d53c34fd0
Tategoto Azarasi <2724167997@qq.com>
2024-09-02 16:19:18 +0800

    Revert "better getColorBilinear"
    This reverts commit 4d4c27b8fc697154b66d4115835de10b0355b494.

--- Texture.hpp
+++ Texture.hpp
@@ -33,27 +33,43 @@ public:
 	Eigen::Vector3f getColorBilinear(float u, float v) {
 		float u_img = u * width;
 		float v_img = (1 - v) * height;
-
-		int x0 = static_cast<int>(floor(u_img));
-		int x1 = x0 + 1;
-		int y0 = static_cast<int>(floor(v_img));
-		int y1 = y0 + 1;
-
-		x1 = std::min(x1, width - 1);
-		y1 = std::min(y1, height - 1);
-
-		float s = u_img - x0;
-		float t = v_img - y0;
-
-		auto color00 = image_data.at<cv::Vec3b>(y0, x0);
-		auto color01 = image_data.at<cv::Vec3b>(y1, x0);
-		auto color10 = image_data.at<cv::Vec3b>(y0, x1);
-		auto color11 = image_data.at<cv::Vec3b>(y1, x1);
-
-		auto color0 = color00 * (1 - s) + color10 * s;
-		auto color1 = color01 * (1 - s) + color11 * s;
-		auto color  = color0 * (1 - t) + color1 * t;
-
+		float du    = u_img - floor(u_img);
+		float dv    = v_img - floor(v_img);
+		cv::Vec2i u00, u01, u10, u11;
+		if(du < 0.5 && dv < 0.5) {// left bottom
+			u00 = cv::Vec2i(floor(u_img - 0.5), floor(v_img - 0.5));
+			u01 = cv::Vec2i(floor(u_img - 0.5), floor(v_img));
+			u10 = cv::Vec2i(floor(u_img), floor(v_img - 0.5));
+			u11 = cv::Vec2i(floor(u_img), floor(v_img));
+		} else if(du < 0.5 && dv >= 0.5) {// left top
+			u00 = cv::Vec2i(floor(u_img - 0.5), floor(v_img));
+			u01 = cv::Vec2i(floor(u_img - 0.5), floor(v_img + 0.5));
+			u10 = cv::Vec2i(floor(u_img), floor(v_img));
+			u11 = cv::Vec2i(floor(u_img), floor(v_img + 0.5));
+		} else if(du >= 0.5 && dv < 0.5) {// right bottom
+			u00 = cv::Vec2i(floor(u_img), floor(v_img - 0.5));
+			u01 = cv::Vec2i(floor(u_img), floor(v_img));
+			u10 = cv::Vec2i(floor(u_img + 0.5), floor(v_img - 0.5));
+			u11 = cv::Vec2i(floor(u_img + 0.5), floor(v_img));
+		} else {//right top
+			u00 = cv::Vec2i(floor(u_img), floor(v_img));
+			u01 = cv::Vec2i(floor(u_img), floor(v_img + 0.5));
+			u10 = cv::Vec2i(floor(u_img + 0.5), floor(v_img));
+			u11 = cv::Vec2i(floor(u_img + 0.5), floor(v_img + 0.5));
+		}
+		if((u00[0] < 0 || u00[0] >= height || u00[1] < 0 || u00[1] >= width) || (u01[0] < 0 || u01[0] >= height || u01[1] < 0 || u01[1] >= width) || (u10[0] < 0 || u10[0] >= height || u10[1] < 0 || u10[1] >= width) || (u11[0] < 0 || u11[0] >= height || u11[1] < 0 || u11[1] >= width)) {
+			auto color = image_data.at<cv::Vec3b>(v_img, u_img);
+			return Eigen::Vector3f(color[0], color[1], color[2]);
+		}
+		auto color00 = image_data.at<cv::Vec3b>(u00[1], u00[0]);
+		auto color01 = image_data.at<cv::Vec3b>(u01[1], u01[0]);
+		auto color10 = image_data.at<cv::Vec3b>(u10[1], u10[0]);
+		auto color11 = image_data.at<cv::Vec3b>(u11[1], u11[0]);
+		float s      = u_img - (static_cast<float>(u00[0]) + 0.5f);
+		auto color0  = color00 * s + color10 * (1 - s);
+		auto color1  = color01 * s + color11 * (1 - s);
+		float t      = v_img - (static_cast<float>(u00[1]) + 0.5f);
+		auto color   = color0 * t + color1 * (1 - t);
 		return Eigen::Vector3f(color[0], color[1], color[2]);
 	}
 };
