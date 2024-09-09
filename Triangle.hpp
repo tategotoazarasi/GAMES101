3c06e27a21c4c306cbca73655353d7a1b4ca3a65
Tategoto Azarasi <2724167997@qq.com>
2024-09-09 20:07:43 +0800

    skeleton for assignment 5
    Signed-off-by: Tategoto Azarasi <2724167997@qq.com>

--- Triangle.hpp
+++ Triangle.hpp
@@ -1,39 +1,77 @@
-//
-// Created by LEI XU on 4/11/19.
-//
+#pragma once
 
-#ifndef RASTERIZER_TRIANGLE_H
-#define RASTERIZER_TRIANGLE_H
+#include "Object.hpp"
 
-#include "Texture.hpp"
-#include <eigen3/Eigen/Eigen>
+#include <cstring>
 
-using namespace Eigen;
-class Triangle {
+bool rayTriangleIntersect(const Vector3f &v0, const Vector3f &v1, const Vector3f &v2, const Vector3f &orig,
+                          const Vector3f &dir, float &tnear, float &u, float &v) {
+	// TODO: Implement this function that tests whether the triangle
+	// that's specified bt v0, v1 and v2 intersects with the ray (whose
+	// origin is *orig* and direction is *dir*)
+	// Also don't forget to update tnear, u and v.
+	return false;
+}
 
+class MeshTriangle: public Object {
 public:
-	Vector4f v[3]; /*the original coordinates of the triangle, v0, v1, v2 in counter clockwise order*/
-	/*Per vertex values*/
-	Vector3f color[3];     //color at each vertex;
-	Vector2f tex_coords[3];//texture u,v
-	Vector3f normal[3];    //normal vector for each vertex
+	MeshTriangle(const Vector3f *verts, const uint32_t *vertsIndex, const uint32_t &numTris, const Vector2f *st) {
+		uint32_t maxIndex = 0;
+		for(uint32_t i = 0; i < numTris * 3; ++i)
+			if(vertsIndex[i] > maxIndex)
+				maxIndex = vertsIndex[i];
+		maxIndex += 1;
+		vertices = std::unique_ptr<Vector3f[]>(new Vector3f[maxIndex]);
+		memcpy(vertices.get(), verts, sizeof(Vector3f) * maxIndex);
+		vertexIndex = std::unique_ptr<uint32_t[]>(new uint32_t[numTris * 3]);
+		memcpy(vertexIndex.get(), vertsIndex, sizeof(uint32_t) * numTris * 3);
+		numTriangles  = numTris;
+		stCoordinates = std::unique_ptr<Vector2f[]>(new Vector2f[maxIndex]);
+		memcpy(stCoordinates.get(), st, sizeof(Vector2f) * maxIndex);
+	}
 
-	Texture *tex = nullptr;
-	Triangle();
+	bool intersect(const Vector3f &orig, const Vector3f &dir, float &tnear, uint32_t &index,
+	               Vector2f &uv) const override {
+		bool intersect = false;
+		for(uint32_t k = 0; k < numTriangles; ++k) {
+			const Vector3f &v0 = vertices[vertexIndex[k * 3]];
+			const Vector3f &v1 = vertices[vertexIndex[k * 3 + 1]];
+			const Vector3f &v2 = vertices[vertexIndex[k * 3 + 2]];
+			float t, u, v;
+			if(rayTriangleIntersect(v0, v1, v2, orig, dir, t, u, v) && t < tnear) {
+				tnear = t;
+				uv.x  = u;
+				uv.y  = v;
+				index = k;
+				intersect |= true;
+			}
+		}
 
-	Eigen::Vector4f a() const { return v[0]; }
-	Eigen::Vector4f b() const { return v[1]; }
-	Eigen::Vector4f c() const { return v[2]; }
+		return intersect;
+	}
 
-	void setVertex(int ind, Vector4f ver);             /*set i-th vertex coordinates */
-	void setNormal(int ind, Vector3f n);               /*set i-th vertex normal vector*/
-	void setColor(int ind, float r, float g, float b); /*set i-th vertex color*/
+	void getSurfaceProperties(const Vector3f &, const Vector3f &, const uint32_t &index, const Vector2f &uv, Vector3f &N,
+	                          Vector2f &st) const override {
+		const Vector3f &v0  = vertices[vertexIndex[index * 3]];
+		const Vector3f &v1  = vertices[vertexIndex[index * 3 + 1]];
+		const Vector3f &v2  = vertices[vertexIndex[index * 3 + 2]];
+		Vector3f e0         = normalize(v1 - v0);
+		Vector3f e1         = normalize(v2 - v1);
+		N                   = normalize(crossProduct(e0, e1));
+		const Vector2f &st0 = stCoordinates[vertexIndex[index * 3]];
+		const Vector2f &st1 = stCoordinates[vertexIndex[index * 3 + 1]];
+		const Vector2f &st2 = stCoordinates[vertexIndex[index * 3 + 2]];
+		st                  = st0 * (1 - uv.x - uv.y) + st1 * uv.x + st2 * uv.y;
+	}
 
-	void setNormals(const std::array<Vector3f, 3> &normals);
-	void setColors(const std::array<Vector3f, 3> &colors);
-	void setTexCoord(int ind, Vector2f uv); /*set i-th vertex texture coordinate*/
-	std::array<Vector4f, 3> toVector4() const;
+	Vector3f evalDiffuseColor(const Vector2f &st) const override {
+		float scale   = 5;
+		float pattern = (fmodf(st.x * scale, 1) > 0.5) ^ (fmodf(st.y * scale, 1) > 0.5);
+		return lerp(Vector3f(0.815, 0.235, 0.031), Vector3f(0.937, 0.937, 0.231), pattern);
+	}
+
+	std::unique_ptr<Vector3f[]> vertices;
+	uint32_t numTriangles;
+	std::unique_ptr<uint32_t[]> vertexIndex;
+	std::unique_ptr<Vector2f[]> stCoordinates;
 };
-
-
-#endif//RASTERIZER_TRIANGLE_H
